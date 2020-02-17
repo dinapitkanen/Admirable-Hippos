e516a37ce7420b5d4f5d17d9fc4f76db93600797
dinapitkanen <58941214+dinapitkanen@users.noreply.github.com>
2020-02-17 18:09:09 +0100

    Update main.js

--- memoryGame/main.js
+++ memoryGame/main.js
@@ -1,1 +1,9 @@
 var robot = request(robotjs);
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 20, 20, 20);
+ctx.stroke(){
+ctx.strokeStyle(#f2250a);
+    
+};
