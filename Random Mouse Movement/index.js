b00c9781d5ace71df5d270be4345fafd7e8c25e3
Axel Bengtsson <bengtsson.axel96@gmail.com>
2020-02-18 14:48:52 +0100

    Created the Random movement of the mouse
    This program has to be run via node with the call 'node indes.js'

---
+++ Random Mouse Movement/index.js
@@ -0,0 +1,44 @@
+const robot = require('robotjs');
+
+let mousePos = robot.getMousePos();
+let newMousePos = { x: 0, y: 0};
+let randomValue = Math.random();
+let mouseMoveTimesCount = 0; 
+
+timerMouseMovement();
+
+function timerMouseMovement () {
+    setTimeout(function () {
+        moveTheMouse();
+        function moveTheMouse () {          
+            setTimeout(function () {
+                mousePos = robot.getMousePos();
+                newMousePos = { x: mousePos.x + randomMovement(), y: mousePos.y + randomMovement()}
+                robot.moveMouse(newMousePos.x, newMousePos.y);       
+                mouseMoveTimesCount++; 
+        
+                if (mouseMoveTimesCount < 30) {           
+                    moveTheMouse();             
+                }                        
+            }, 100);
+        }
+        mouseMoveTimesCount = 0;
+        if (true){
+            timerMouseMovement();
+        }
+    }, 50000);
+}
+
+
+      
+function randomMovement () {
+    randomValue = Math.random();
+
+    if ( randomValue < 0.6){
+        console.log('High');
+        return randomValue * 6;
+    }else {
+        console.log('Low');
+        return randomValue * -6;
+    }
+}
