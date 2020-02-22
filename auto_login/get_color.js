757348ff5365d5e098489804c3fcc79a31970c4d
Kreosot <max.derlow@gmail.com>
2020-02-22 13:07:35 +0100

    refactored file names, removed redundancies, re-packaged dependencies

--- auto_login/get_color.js
+++
@@ -1,7 +0,0 @@
-const robot = require("robotjs");
-let x = robot.getMousePos().x;
-let y = robot.getMousePos().y;
-console.log("color: " + robot.getPixelColor(x, y) + " x: " + x + " y: " + y);
-
-// bright color: dc8a00
-// dark color: 6b43000
