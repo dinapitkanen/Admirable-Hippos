4340178f8dedaa98b79ad07c3e2efb92924a37e5
dinapitkanen <58941214+dinapitkanen@users.noreply.github.com>
2020-02-24 17:04:52 +0100

    refinement of code and README

--- color_pipette/main.js
+++ color_pipette/main.js
@@ -1,17 +1,16 @@
-// Get pixel color under the mouse.
+// require API
 var robot = require("robotjs");
 
-// Get mouse position.
-
-
+// This calls the function
 colorPipette();
 
-
+// This is the core of the pipette declaring what the function will do.
+// Get pixel color in hex format and read it every second until you stop the code.
 
 function colorPipette(){
     let timer = setInterval (function() {
         let mouse = (robot.getMousePos())
-// Get pixel color in hex format.
+
     var hex = robot.getPixelColor(mouse.x, mouse.y);
     console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
     }, 1000) ;
