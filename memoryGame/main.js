9079adeb9be690357cf0746598fe40f6429fb97d
dinapitkanen <58941214+dinapitkanen@users.noreply.github.com>
2020-02-24 15:56:19 +0100

    Update README

--- memoryGame/main.js
+++ memoryGame/main.js
@@ -1,9 +1,22 @@
-var robot = request(robotjs);
+
+// draws a red solid 150 x 100 rectangle
+
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.rect(20, 20, 20, 20);
-ctx.stroke(){
-ctx.strokeStyle(#f2250a);
-    
-};
+    ctx.beginPath();
+    ctx.rect(20, 20, 150, 100);
+    ctx.lineWidth = "4";
+    ctx.strokeStyle = "#f2250a";
+    ctx.stroke();
+    ctx.delay(time).fadeOut(300);
+
+
+// robotjs reads mouse check points a 20, 150, 100, 20 
+
+// example code from zyntax https://github.com/octalmage/robotjs 
+
+var robot = require("robotjs");
+
+var mouse = robot.getMousePos();
+console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
+scrollMouse(x, y);
