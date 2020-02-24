6256467a2aec2e97ade49e75ea0122274c7cc165
dinapitkanen <58941214+dinapitkanen@users.noreply.github.com>
2020-02-24 10:49:40 +0100

    added more color shades because color varies depending on something

--- theEndOfTheRoad/main.js
+++ theEndOfTheRoad/main.js
@@ -50,11 +50,11 @@ let mousePixelColor;
 let i = 5;
 
 //robot.moveMouseSmooth(0, 0);
-//console.log(robot.getPixelColor(robot.getMousePos().x, robot.getMousePos().y));
+console.log(robot.getPixelColor(robot.getMousePos().x, robot.getMousePos().y));
 
 //This function will be called when we run the .js file. This function also starts the game.
 
-startGameTimer();
+//startGameTimer();
 
 //This function logs a countdown from 5 to 1 and then calls the 'checkMouseInLine()' and stops the countdown
 function startGameTimer(){
@@ -87,14 +87,14 @@ function checkMouseInLine(){
         mousePixelColor = robot.getPixelColor(robot.getMousePos().x, robot.getMousePos().y);
 
         //If the color of the pixel is green it's a win and the program closes
-        if (mousePixelColor == '00b800'){
+        if (mousePixelColor == '00b800' || '00b700'){
             console.log('You WON');
             clearInterval(timer);
         }
 
         //If the color of the pixel is not green or light blue it means that the mouse is outside of the course and you fail
         //The timer starts over and you get a second chance
-        else if (mousePixelColor == '1f2a5a'){
+        else if (mousePixelColor == '1f2a5a' || '1f2e57'){
             console.log('You FAILED, start over');
             //robot.moveMouse(183, 325);
             i = 5;
