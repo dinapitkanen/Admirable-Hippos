d7c1886779f229d3bdc2440edac8427929cbd39a
dinapitkanen <58941214+dinapitkanen@users.noreply.github.com>
2020-02-24 16:50:02 +0100

    changes in code

--- color_pipette/main.js
+++ color_pipette/main.js
@@ -2,10 +2,17 @@
 var robot = require("robotjs");
 
 // Get mouse position.
-var mouse = robot.getMousePos();
 
+
+colorPipette();
+
+
+
+function colorPipette(){
+    let timer = setInterval (function() {
+        let mouse = (robot.getMousePos())
 // Get pixel color in hex format.
-var hex = robot.getPixelColor(mouse.x, mouse.y);
-console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
-
-
+    var hex = robot.getPixelColor(mouse.x, mouse.y);
+    console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
+    }, 1000) ;
+};
